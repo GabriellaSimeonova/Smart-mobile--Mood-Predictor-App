33b3c7fef34bdc6379c7eecac83310c0e7954393
Gabriela Simeonova <g.simeonova@student.fontys.nl>
2023-02-27 12:27:25 +0100

    sw file and cache: offline page added

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
 // Set up a cache with the resources we want to cache
 const cacheName = 'v1';
 const resourcesToPrecache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/ICON.png'
+  '/offline.html',
+  '/ICON.png',
+  
 ];
 
 // Listen for the install event, which occurs when the service worker is first installed
@@ -14,34 +12,28 @@ self.addEventListener("install", (event) => {
 
   event.waitUntil(
       
-      (async() => {
-          try {
-              cache_obj = await caches.open(cache)
-              cache_obj.addAll(caching_files)
-          }
-          catch{
-              console.log("error occured while caching...")
-          }
-      })()
+    caches.open(cacheName).then((cache)=>{
+      console.log("Service Worker: Caching important offline files");
+      return cache.addAll(resourcesToPrecache);
+    })
   )
 } )
 // Listen for the fetch event, which is triggered for every network request the app makes
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return fetch(event.request)
-          .then((response) => {
-            return caches.open(cacheName)
-              .then((cache) => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-          });
+    caches.match(event.request.url).then((response)=>{
+      console.log("Service Worker: Fetching resource "+event.request.url);
+      return response||fetch(event.request).then((response)=>{
+        console.log("Service Worker: Resource "+event.request.url+" not available in cache");
+        return caches.open(cacheName).then((cache)=>{
+          console.log("Service Worker: Caching (new) resource "+event.request.url);
+          cache.put(fetching.request,response.clone());
+          return response;
+        });
+      }).catch(function(){      
+        console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+        //Do something else with the request (for example: respond with a different cached file)
       })
+    })
   );
 });
