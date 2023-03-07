fdac5c52f0b1bd18f69cab7885a9c298ca3e1562
Gabriela Simeonova <g.simeonova@student.fontys.nl>
2023-03-08 00:07:44 +0100

    PWA basics- manifest and sw file

--- src/index.js
+++ src/index.js
@@ -16,3 +16,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+      console.log('Successfully registered service worker', reg);
+  }).catch(function(err) {
+      console.warn('Error whilst registering service worker', err);
+  });
+  }
