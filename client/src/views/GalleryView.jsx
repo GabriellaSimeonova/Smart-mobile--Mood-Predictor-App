4e442b73db84ceb35e3f0019530dba36a82fb6bb
Stoil Yonchev <I478992@fhict.nl>
2023-03-14 23:27:30 +0100

    Little formating :sparkles:

--- client/src/views/GalleryView.jsx
+++ client/src/views/GalleryView.jsx
@@ -6,24 +6,6 @@ import Jabimodo from '../resources/pics/Jabimodo.jpeg';
 
 import PhotoCard from '../components/PhotoCard';
 
-const GalleryContainer = styled.div`
-  max-height: 92.1vh;
-  overflow-y: scroll;
-`;
-
-const GalleryWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
-
-const PhotoCardWrapper = styled.div`
-  flex-basis: 33.33%; /* for larger screens, display 3 pictures per row */
-  @media screen and (max-width: 768px) {
-    flex-basis: 50%; /* for smaller screens like phones, display 2 pictures per row */
-  }
-`;
-
 export default function PhotoGallery() {
   const [cards, setCards] = useState([
     { id: 1, img: Jabi, mood: 'relaxed' },
@@ -44,5 +26,21 @@ export default function PhotoGallery() {
   );
 }
 
+const GalleryContainer = styled.div`
+  max-height: 92.1vh;
+  overflow-y: scroll;
+`;
 
+const GalleryWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const PhotoCardWrapper = styled.div`
+  flex-basis: 33.33%; /* for larger screens, display 3 pictures per row */
+  @media screen and (max-width: 768px) {
+    flex-basis: 50%; /* for smaller screens like phones, display 2 pictures per row */
+  }
+`;
 
